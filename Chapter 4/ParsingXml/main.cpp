33307eb84528be8791c0a0de1b1986d158058b45
Virion <leezhieng@gmail.com>
2019-12-09 20:04:07 +0800

    Update main.cpp

--- Chapter 4/ParsingXml/main.cpp
+++ Chapter 4/ParsingXml/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 	QCoreApplication a(argc, argv);
 
 	WorkerThread* thread = new WorkerThread(&a);
-	thread->fetch(""https://raw.githubusercontent.com/PacktPublishing/Application-Development-with-Qt-Creator-Third-Edition/master/Chapter%204/ParsingXml/sample.xml""); // Your XML file here
+	thread->fetch("https://raw.githubusercontent.com/PacktPublishing/Application-Development-with-Qt-Creator-Third-Edition/master/Chapter%204/ParsingXml/sample.xml"); // Your XML file here
 
 	return a.exec();
 }
